f85715aff15db4cf4746415e95dc0f2c29daf7a2
carsonpepe <43485192+carsonpepe@users.noreply.github.com>
2018-09-24 13:53:29 -0700

    Update planets.py
    I committed these exact changes to this file from SSH and it said it was committed, however the commit doesn't show up on git. I went ahead and typed the changes in manually on here.

--- planets.py
+++ planets.py
@@ -1,17 +1,19 @@
 #
-#Carson Pepe
-#13926584
-#9/23/2018
+# Name: Carson Pepe
+# Student ID: 13926584
+# 9/24/2018
 #
-#Lab 0
-#Section 15
-#Purpose: To shortly practice previously learned python skills while writing concise code.
+# Lab 0
+# Section 13
+# Purpose of Lab: To practice writing more efficient code.
 
 def weight_on_planets():
+
+   # Prompts user for their weight, casts input into an integer, then stored in a var
    weight = int(input("What do you weigh on earth? "))
-   mars = weight * 0.38
-   jupiter = weight * 2.34
-   print("\nOn Mars you would weigh", mars, "pounds.\nOn Jupiter you would weigh", jupiter, "pounds."  
-   
+
+   # Prints output and uses the variable to multiple by appropriate number
+   print("\nOn Mars you would weigh", weight * 0.38, "pounds.\nOn Jupiter you would weigh", weight * 2.34, "pounds.")
+
 if __name__ == '__main__':
    weight_on_planets()
